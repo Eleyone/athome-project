ef9d93f3bbb38d2608f556d4fe9c2a23006b86fd
Eleyone <eleyone@gmail.com>
2016-02-17 10:17:58 +0100

    Refactoring to simplify un-understand module arch in Node.JS

--- server.js
+++ server.js
@@ -7,9 +7,9 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
+var express = require('express');        // call express
 var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
+var mongoose = require('mongoose');
 
 var port = process.env.PORT || 8080;        // set our port
 var app = express();                 // define our app using express
@@ -27,12 +27,70 @@ var router = express.Router();
 
 // Initial dummy route for testing
 // http://localhost:3000/api
-router.get('/', function(req, res) {
-    res.json({ message: 'You are running dangerously low on beer!' });
+router.get('/', function (req, res) {
+    res.json({message: 'You are running dangerously low on beer!'});
 });
 app.use('', router);
 
-var bookApiRouter = require("./app.js/modules/book-api");
+var bookApiRouter = router.route("/api");
+
+var Book = require("./book.js");
+
+var booksRoute = bookApiRouter.route('/books');
+
+booksRoute.get(function (req, res) {
+    Book.find(function (err, books) {
+        if (err)
+            res.send(err);
+        res.json(books);
+    });
+});
+
+booksRoute.post(function (req, res) {
+    // try to use high-level calls here
+    // if you want something complex just create another special module for this
+    var newBook = new Book(req.body);
+    newBook.save(function (err, book) {
+        if (err)
+            res.send(err); // do something on error
+        res.json(book); // return user json if ok
+    });
+});
+
+var bookRoute = bookApiRouter.route('/books/:book_id');
+
+bookRoute.get(function (req, res) {
+    Book.findOne(req.params.book_id, function (err, book) {
+        if (err)
+            res.send(err);
+        res.json(book);
+    });
+});
+
+bookRoute.put(function (req, res) {
+    Book.findById(req.params.book_id, function (err, book) {
+        // Update the existing book attributes
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.isbn = req.body.isbn;
+        book.resume = req.body.resume;
+
+        book.save(function (err, book) {
+            if (err)
+                res.send(err);
+            res.json(book);
+        });
+    });
+});
+
+bookRoute.delete(function (req, res) {
+    Book.findByIdAndRemove(req.params.book_id, function (err) {
+        if (err)
+            res.send(err);
+        res.json({message: 'Book delete'});
+    });
+});
+
 app.use('/api', bookApiRouter);
 //app.use(require("static-pages"));
 
